2440e4ee4a550dbba715378df42c987d41e1f742
Dhia <147658538+m-dhia@users.noreply.github.com>
2024-05-26 13:31:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://4.233.223.134:4200"
+  origin: "http://20.19.33.63:4200/:4200"
 };
 
 app.use(cors(corsOptions));
